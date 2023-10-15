e8d8d487508a045981e88d325cc9c9c678546f9f
Daniel Rochon <drochon891@gmail.com>
2023-10-15 00:18:01 -0400

    created more basic templates and added bootstrap

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 import sqlite3
 
 app = Flask(__name__)
+
 
 @app.route("/job_list")
 def job_list():
@@ -13,3 +14,21 @@ def job_list():
     rows = cur.fetchall()
     con.close()
     return render_template('job_list.html', rows=rows)
+
+@app.route('/job/<int:job_id>')
+def job_detail(job_id):
+    con = sqlite3.connect('./Scraper/Data/archive.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+
+    cur.execute("SELECT * FROM JOB WHERE ID = ?", (job_id,))
+    job = cur.fetchone()
+
+    con.close()
+    
+    if job:
+        return render_template('job_detail.html', job=job)
+    else:
+        return "Job not found", 404
+
+
