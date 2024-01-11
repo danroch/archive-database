f733d9e920f0e15f3628e371a48ea3774b98b2c4
Daniel Rochon <drochon891@gmail.com>
2024-01-10 20:44:18 -0500

    sql queries

--- app.py
+++ app.py
@@ -31,4 +31,26 @@ def job_detail(job_id):
     else:
         return "Job not found", 404
 
+@app.route('/employer/<int:employer_id>')
+def employer_detail(employer_id):
+    con = sqlite3.connect('./Scraper/Data/archive.db')
+    con.row_factory = sqlite3.Row
+    cur = con.cursor()
+    query = f'''
+    SELECT e.ID, AVG(ev.JOB_SATISFACTION) as avg_job_satisfaction, AVG(ev.TRAINING) AS avg_training
+    FROM EMPLOYER AS e
+    LEFT JOIN JOB AS j on e.ID = j.EMPLOYER_ID
+    LEFT JOIN EVALUATION AS ev ON j.ID = ev.JOB_ID
+    WHERE e.ID = {employer_id};
+    '''
+    cur.execute(query)
+    employer = cur.fetchone()
 
+    if employer:
+        avg_job_satisfaction = f"{employer['avg_job_satisfaction']: .2f}"
+        return render_template('employer_detail.html', employer=employer, avg_job_satisfaction=avg_job_satisfaction)
+    else:
+        return "Employer not found", 404
+
+if __name__=='__main__':
+    app.run(debug=True)
